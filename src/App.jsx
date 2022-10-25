d8aecf49a03b8735e060a800b88f72cbbe516821
Hojjat Jokar <paintthedream@gmail.com>
2022-10-25 19:44:26 +0200

    chore: clean up the boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      Header
+
+      <div className="grid">
+        sortedTodos
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;

e1c8af0c5455796b7f76906479bee7046ff875bb
Hojjat Jokar <paintthedream@gmail.com>
2022-10-25 20:06:25 +0200

    doc: add question comments

--- src/http/api-client.js
+++ src/http/api-client.js
@@ -4,6 +4,7 @@ export const ApiClient = {
   get: async (url) => {
     const response = await fetch(url);
     const data = await response.json();
+    // @TODO Question: why setting this in config? 
     config.todos = data.results;
     return data;
   }
--- src/http/repository.js
+++ src/http/repository.js
@@ -8,13 +8,14 @@ import { ApiClient } from "./api-client";
 //       console.info("server request sent.");
 //       return ApiClient.get("https://randomuser.me/api/?results=40");
 //     },
-//     // Q: why we need this? 
+//     // @TODO: Question: why we need this? 
 //     { refetchOnWindowFocus: false }
 //   );
 // };
 
 export const useGetTodos = () => {
   const query = useQuery(['todos'], ()=> {
+    // @TODO: the domain name, endpoint and the 40 is hardcode, why?
     return ApiClient.get("https://randomuser.me/api/?results=40");
   });
 
